97b633bade2f77293207ebc4a258c2750f9288a4
Steven James <sjames@wlapcs.org>
2017-04-11 17:28:28 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var steve = 300;
+var anna = 200;
+var x = 0;
+
+function setup(){
+  createCanvas(600,400);
+}
+
+function draw(){
+  background(50,50,50,50);
+  display();
+  stroke(350);    
+}
+ 
+if(mouseX > 100){
+
+}
+
+else{
+ background(0);
+
+    
+}
+
+    
+
+function display(){
+    ellipse(steve,anna,200,200);
+    rect(150,50,50,50);
+    line(200,200,200,200);
+}
+
+function paint(x,y){
+    
+}
